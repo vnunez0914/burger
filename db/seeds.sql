f26393e9bd021d2d129ca45bf305f5206eed76ee
Victor Nunez <vnunez0914@gmail.com>
2019-10-17 21:52:08 -0500

    database and connection set up

---
+++ db/seeds.sql
@@ -0,0 +1,8 @@
+INSERT INTO burgers (burger_name, devoured)
+VALUES ("Cheeseburger", TRUE);
+
+INSERT INTO burgers (burger_name, devoured)
+VALUES ("Turkey Burger", TRUE);
+
+INSERT INTO burgers (burger_name, devoured)
+VALUES ("Chicken Burger", FALSE);
