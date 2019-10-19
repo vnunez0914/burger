615bfc9ec92ec87840806a1306de644b7f5112b5
Victor Nunez <vnunez0914@gmail.com>
2019-10-18 23:01:47 -0500

    added object to the router.get route

--- controllers/burger_controller.js
+++ controllers/burger_controller.js
@@ -5,8 +5,12 @@ var burger = require('../models/burger.js')
 // reference to burger.js file in models and calling the burger table passing the data in the function
 router.get("/", function(req, res){
     burger.all(function(data){
+        var hbsObject = {
+            burger: data
+        }
         console.log(data)
-        res.render('index');
+        console.log(hbsObject)
+        res.render('index', hbsObject);
     })
 })
 
