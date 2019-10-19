5ec93bafdefd955457be515896b839336387d55d
Victor Nunez <vnunez0914@gmail.com>
2019-10-19 17:17:58 -0500

    index #each loop created and update, create, delete functions added to burger.js

--- controllers/burger_controller.js
+++ controllers/burger_controller.js
@@ -4,14 +4,19 @@ var burger = require('../models/burger.js')
 
 // reference to burger.js file in models and calling the burger table passing the data in the function
 router.get("/", function(req, res){
-    burger.all(function(data){
-        var hbsObject = {
-            burger: data
-        }
-        console.log(data)
-        console.log(hbsObject)
-        res.render('index', hbsObject);
+    burger.all(function(bData){
+        console.log(bData)
+        
+        res.render('index', {bData} );
     })
+})
+
+// update burger
+router.put('/burgers/update',function(eq, res){
+    burger.update(req.body.burger_id, function(result){
+        console.log(result);
+        res.redirect('/');
+    });
 })
 
 module.exports = router
--- models/burger.js
+++ models/burger.js
@@ -1,13 +1,27 @@
-var orm = require("../config/orm.js")
+var orm = require("../config/orm.js");
 
 // calling table orm. and inserting burger table and function call back
 var burger = {
-    all: function(cb){
-        orm.all('burgers', function(res){
-            cb(res);
-        
-        })
-    }
-}
+  all: function(cb) {
+    orm.all("burgers", function(res) {
+      cb(res);
+    });
+  },
+  create: function(cols, vals, cb) {
+    orm.insertOne("burgers", cols, vals, function(res) {
+      cb(res);
+    });
+  },
+  update: function(id, cb) {
+    orm.updateOne("burgers", id, cb, function(res) {
+      cb(res);
+    });
+  },
+  delete: function(condition, cb) {
+    orm.delete("burgers", condition, function(res) {
+      cb(res);
+    });
+  }
 
-module.exports = burger
+};
+module.exports = burger;
